5378b3bc29cb87dac8de920b78219c9d3ff6f1e2
Broveku <gearhearjordan@gmail.com>
2021-07-26 10:21:27 -0700

    base component built

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Signup() {
+
+
+
+
+
+
+const Signup = ()=> {
+const [name, setName] = useState('')
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+
+
     return (
         <div>
-            
+            <h1>Welcome, please signup below!</h1>
+          <form>
+            <label>
+                Name: <input 
+                type='text' 
+                placeholder='First and Last' 
+                value={name} 
+                onChange={(evt)=>{
+                    setName(evt.target.value)  
+             }} ></input>
+            </label>
+            <label>
+                Email: <input 
+                type='email' 
+                placeholder='example@email.com' 
+                value={email} 
+                onChange={(evt)=> {
+                    setEmail(evt.target.value)  
+             }}></input>
+            </label>
+            <label>
+                Password: <input 
+                type='password' 
+                value={password} 
+                onChange={(evt)=> {
+                    setPassword(evt.target.value)  
+             }}></input>
+            </label>
+            <button>submit</button>
+          </form>  
         </div>
     )
 }
