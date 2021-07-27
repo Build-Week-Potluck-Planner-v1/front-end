c9115ec19aa18d2668532962bf0dff891f4c9377
Broveku <gearhearjordan@gmail.com>
2021-07-27 10:35:26 -0700

    added cypress testing for signup and login

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,20 +6,20 @@ import schema from '../validation/formSchema'
 const initialFormValues = {
     username: '',
     password: '',
-    code: '',
+    
 }
 
 const initialFormErrors = {
     username: '',
     password: '',
-    code: '',
+    
 
 }
 
 function Signup() {
 const [username, setName] = useState('')
 const [password, setPassword] = useState('')
-const [code, setCode] = useState('')
+
 const [formValues, setFormValues] = useState(initialFormValues)
 const [formErrors, setFormErrors] = useState(initialFormErrors)
 
@@ -45,7 +45,7 @@ const formSubmit = () =>{
     const newAccount = {
         username: formValues.username.trim(),
         password: formValues.password.trim(),
-        code: formValues.code.trim(),
+        
         
     }
         
@@ -60,23 +60,6 @@ const inputChange = (name, value) =>{
     })
 }
 
-function validateNumbers(evt) {
-    var theEvent = evt || window.event;
-  
-    // Handle paste
-    if (theEvent.type === 'paste') {
-        key = evt.clipboardData.getData('text/plain');
-    } else {
-    // Handle key press
-        var key = theEvent.keyCode || theEvent.which;
-        key = String.fromCharCode(key);
-    }
-    var regex = /[0-9]|\./;
-    if( !regex.test(key) ) {
-      theEvent.returnValue = false;
-      if(theEvent.preventDefault) theEvent.preventDefault();
-    }
-  }
 
     return (
         <div>
@@ -84,7 +67,7 @@ function validateNumbers(evt) {
             <div className='errors'>
                 <div>{formErrors.username}</div>
                 <div>{formErrors.password}</div>
-                <div>{formErrors.code}</div>
+                
             </div>
           <form className='form-container' onSubmit={(evt)=>{
               evt.preventDefault()
@@ -117,27 +100,8 @@ function validateNumbers(evt) {
              }}></input>
             </label>
         </div>
-        <div className='planner-code'>  
-            <label>
-                Planner Code: <input 
-                type='text'
-                placeholder='Please enter a 4 digit code'
-                onKeyPress={(evt)=>{
-                    validateNumbers(evt)
-                }}
-                minLength='4'
-                maxLength='4'
-                required
-                name='code' 
-                value={code} 
-                onChange={(evt)=> {
-                    setCode(evt.target.value)
-                    inputChange(evt.target.name, evt.target.value) 
-                    
-             }}></input>
-            </label>
-        </div>
-            <button>submit</button>
+        
+            <button id='button'>submit</button>
 
           </form>  
         </div>
