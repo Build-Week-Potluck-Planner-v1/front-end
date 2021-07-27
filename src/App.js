a748e7b3dbe7ca6de6bd0c14e8a00a10de057710
Jie Zhang <zhangjiew@gmail.com>
2021-07-27 11:56:20 -0600

    added @media

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import './App.css';
-import {Switch, Link, Route} from 'react-router-dom';
-import Homepage from './components/Homepage';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import React from 'react'
+import "./App.css";
+import { Switch, Link, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-    
-    <nav>
-        <Link to = '/'>Home</Link>
-        <Link to = 'login'>Login</Link>
-        <Link to = 'signup'>Sign Up</Link>
-    </nav>
+      <Header />
 
-    <Switch>
-      <Route path = '/signup'>
-          <Signup/>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
         </Route>
-        <Route path = '/login'>
-          <Login/>
+        <Route path="/login">
+          <Login />
         </Route>
-        <Route exact path = '/'>
-          <Homepage/>
+        <Route exact path="/">
+          <Homepage />
         </Route>
       </Switch>
     </div>
