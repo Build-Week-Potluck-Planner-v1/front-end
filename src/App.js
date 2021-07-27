ab14414263e37887db36ff13bb5790bb9f705505
michaelquintdev <michaelquintdev@gmail.com>
2021-07-27 11:59:29 -0500

    Added dashboard and nav items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Switch, Link, Route} from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
 import React from 'react'
 
 function App() {
@@ -10,12 +12,15 @@ function App() {
     <div className="App">
     
     <nav>
+        <Link to = '/'>Potluck Planner</Link>
         <Link to = '/'>Home</Link>
-        <Link to = 'login'>Login</Link>
-        <Link to = 'signup'>Sign Up</Link>
+        <Link to = '/login'>Login</Link>
+        <Link to = '/signup'>Sign Up</Link>
+        <Link to = '/dashboard'>Dashboard</Link>
     </nav>
 
     <Switch>
+      <PrivateRoute path = '/dashboard' component={Dashboard} />
       <Route path = '/signup'>
           <Signup/>
         </Route>
