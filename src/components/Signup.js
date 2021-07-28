9c8457b2dae47ad1b3c8f40dda3c6bf10f46bae8
Broveku <gearhearjordan@gmail.com>
2021-07-28 11:34:14 -0700

    small css styling with styled components on signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,6 +15,43 @@ const initialFormErrors = {
     password: '',
 }
 
+const FormStyle = styled.form`
+    width:80%;
+    max-width:40rem;
+    margin: 2rem auto;
+    padding: 2rem;
+
+    & .username, .password {
+        margin: 1rem 0;
+        display:flex;
+        align-items:stretch;
+        flex-direction:column;
+    }
+    & label {
+        font-weight:bold;
+        flex:1;
+        color: #464646;
+        margin-bottom:1.0rem
+    }
+    & input {
+        flex:3;
+        font:inherit;
+        padding:0.35rem 0.35rem;
+        border-radius:6px;
+        border: 1px solid #ccc;
+    }
+    & input:focus {
+        outline: none;
+        border-color: #4f005f;
+        background: #f6dbfc;
+      }
+    .errors{
+        margin: 8px;
+        font-size: 0.7em;
+        color: red;
+
+ }
+`
 
 
 function Signup() {
@@ -68,9 +105,8 @@ const inputChange = (name, value) =>{
             <div className='errors'>
                 <div>{formErrors.username}</div>
                 <div>{formErrors.password}</div>
-                
             </div>
-          <form onSubmit={(evt)=>{
+          <FormStyle onSubmit={(evt)=>{
               evt.preventDefault()
               formSubmit()
           }}>
@@ -78,7 +114,7 @@ const inputChange = (name, value) =>{
             <label>Username:</label>
                  <input 
                 type='text' 
-                placeholder='Please enter username'
+                placeholder='Please enter a username'
                 name='username' 
                 value={username} 
                 onChange={(evt)=>{
@@ -104,7 +140,7 @@ const inputChange = (name, value) =>{
         
             <button id='button'>submit</button>
 
-          </form>  
+          </FormStyle>  
         </div>
     )
 }
